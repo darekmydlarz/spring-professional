761bc6e72eeff94229753aa7a1937c42342393c5
Dariusz Mydlarz <dmydlarz@gmail.com>
2020-07-24 22:20:48 +0200

    01 - Rendering HTML Pages

---
+++ src/main/java/com/marcobehler/ApplicationLauncher.java
@@ -0,0 +1,23 @@
+package com.marcobehler;
+
+import org.apache.catalina.Context;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.Wrapper;
+import org.apache.catalina.startup.Tomcat;
+
+public class ApplicationLauncher {
+
+    public static void main(String[] args) throws LifecycleException {
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+        tomcat.getConnector();
+
+        Context context = tomcat.addContext("", null);
+        Wrapper servlet = Tomcat.addServlet(context, "myFirstServlet", new MyFirstServlet());
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/*");
+
+        tomcat.start();
+    }
+
+}
